8a0e6d389d7d51ce71b5dd55903f3553ecc86169
edwin <eramirezn23@icloud.com>
2025-03-31 18:58:45 +0200

    se ha habilitado el editar texto cuando se clickee en el texto de la tarea.

--- app/src/main/java/com/example/todolist/activities/MainActivity.kt
+++ app/src/main/java/com/example/todolist/activities/MainActivity.kt
@@ -33,13 +33,25 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         taskDAO = TaskDAO(this)
+        // asignamos al adapter la lista de tareas vacia y una funcion lambda que se ejecuta cuando se hace click en un elemento del recycler view y recibe la posicion del elemento
+        // en el recycler view
 
-        adapter = TaskAdapter(emptyList()) {
+// usamos las func Lambda para que al darle click en un texto , podamos modificar el texto de la tarea.
+        adapter = TaskAdapter(emptyList()) { position ->
+            // obtenemos la tarea que se ha pulsado en el recycler view apartir de su posicion
+            val task = taskList[position]
 
+            //
+            val intent = Intent(this, TaskActivity::class.java)
+            intent.putExtra(TaskActivity.TASK_ID, task.id)
+            startActivity(intent)
         }
+        // asignamos el adapter al recycler view
         binding.recyclerView.adapter = adapter
+        // asignamos el layout manager al recycler view
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
 
+        // asignamos el listener al boton de añadir tarea
         binding.addTaskButton.setOnClickListener {
             val intent = Intent(this, TaskActivity::class.java)
             startActivity(intent)
--- app/src/main/java/com/example/todolist/activities/TaskActivity.kt
+++ app/src/main/java/com/example/todolist/activities/TaskActivity.kt
@@ -11,10 +11,14 @@ import com.example.todolist.data.TaskDAO
 import com.example.todolist.databinding.ActivityTaskBinding
 
 class TaskActivity : AppCompatActivity() {
+    companion object {
+        const val TASK_ID = "TASK_ID"
+    }
 
     lateinit var binding: ActivityTaskBinding
-
     lateinit var taskDAO: TaskDAO
+    // creamos task de tipo Task para poder acceder a sus propiedades
+    lateinit var task: Task
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,17 +32,35 @@ class TaskActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        //recuperamos el id de la tarea que se ha pulsado en el recycler view
+        //RECIBIMOS EL EXTRA DEL MAIN ACTIVITY
+        val taskId = intent.getLongExtra(TASK_ID, -1)// asignamos -1 porque  si no tiene nada el id es -1 entonces seria una tarea nueva,
+        // si lo tiene es una tarea existente
 
         taskDAO = TaskDAO(this)
+// hacmeos un if para saber si la tarea es nueva o si es una tarea existente
+        if(taskId != -1L)// significa que es una tarea existente
+        {
+            // se crea una tarea nueva con el valor de la tarea existente buscando el ID en la base de datos
+            task = taskDAO.findById(taskId)!!
+            binding.titleEditText.setText(task.title)// ese valor que ya tenia se pone por defecto en el editText y asi la podremos editar y guardar.
+        }else{
+            task = Task(-1L, "")// significa que es una tarea nueva entonces creamos una nueva tarea con un id de -1 y un titulo vacio
+        }
 
         binding.saveButton.setOnClickListener {
-            val title = binding.titleEditText.text.toString()
+            val title = binding.titleEditText.text.toString()// obtenemos el titulo que se ha escrito en el editText
+            task= Task(task.id, title)// creamos una nueva tarea con el titulo que se ha escrito en el editText
 
-            val task = Task(-1L, title)
 
-            taskDAO.insert(task)
-
-            finish()
+            // es aqui donde usamos el -1 .... si es -1 significa que es una tarea nueva y si no es -1 significa que es una tarea existente
+            // si es -1 lo guardamos en la base de datos y si no lo actualizamos
+            if (task.id != -1L) {
+                taskDAO.update(task)
+            } else {
+                taskDAO.insert(task)
+            }
+            finish()//cierra la actividad y nos devuelve a la actividad anterior
         }
     }
 }
