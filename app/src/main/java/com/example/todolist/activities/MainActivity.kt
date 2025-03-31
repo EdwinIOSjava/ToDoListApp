923e260cef620dd9cff89e428f963f78d104d3f2
edwin <eramirezn23@icloud.com>
2025-03-31 19:42:45 +0200

    agregado el AlertDiaglog.Builder y modifciadas las fun lambdas para entenderlas.

--- app/src/main/java/com/example/todolist/activities/MainActivity.kt
+++ app/src/main/java/com/example/todolist/activities/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.todolist.activities
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -37,23 +38,19 @@ class MainActivity : AppCompatActivity() {
         // en el recycler view
 
 // usamos las func Lambda para que al darle click en un texto , podamos modificar el texto de la tarea.
-        adapter = TaskAdapter(emptyList(), { position ->
-            // obtenemos la tarea que se ha pulsado en el recycler view apartir de su posicion
-            val task = taskList[position]
+        adapter = TaskAdapter(
+            emptyList(),
+            { position -> // esta es la primera funcion Lambda que se ejecuta cuando se hace click en un elemento del recycler view y recibe la posicion del elemento en el recycler view
+                // obtenemos la tarea que se ha pulsado en el recycler view apartir de su posicion
+                val task = taskList[position]
 
-            //
-            val intent = Intent(this, TaskActivity::class.java)
-            intent.putExtra(TaskActivity.TASK_ID, task.id)
-            startActivity(intent)
-        },{position ->// AQUI VA LA SEGUNDA FUNC LAMBDA que usaremos para Borrar la tarea al presionar el boton de borrar
+                val intent = Intent(this, TaskActivity::class.java)
+                intent.putExtra(TaskActivity.TASK_ID, task.id)// aqui le pasamos el id de la tarea que se ha pulsado en el recycler view a la actividad TaskActivity para que pueda ser editada
+                startActivity(intent)
+            },
 
-            val task = taskList[position] // obtenemos la tarea que se ha pulsado en el recycler view apartir de su posicion
-
-            taskDAO.delete(task) // usamos la funcion delete de la clase TaskDAO para borrar la tarea de la base de datos
-
-            refreshData() // por ultimo llamamos a la funcion que refresca los datos del recycler view
-
-        })
+                ::deleteTaskFuntionLambda // esta es la seguda funcion Lambda pero la hemos  bautizado y creado a lo ultimo para ejemplificar que se puede hacer asi
+            )
         // asignamos el adapter al recycler view
         binding.recyclerView.adapter = adapter
         // asignamos el layout manager al recycler view
@@ -65,6 +62,7 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
     override fun onResume() {
         super.onResume()
 
@@ -72,8 +70,30 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun refreshData(){
-        taskList = taskDAO.findAll()// usamos la funcion findAll de la clase TaskDAO para obtener todas las tareas de la base de datos
+    private fun refreshData() {
+        taskList =
+            taskDAO.findAll()// usamos la funcion findAll de la clase TaskDAO para obtener todas las tareas de la base de datos
         adapter.updateItems(taskList)// usamos la funcion updateItems de la clase TaskAdapter para actualizar los datos del recycler view
+    }
+    fun editTaskFuntionLambda(position: Int){
+
+    }
+    fun deleteTaskFuntionLambda(position: Int){
+        // AQUI VA LA SEGUNDA FUNC LAMBDA que usaremos para Borrar la tarea al presionar el boton de borrar
+
+        val task =taskList[position] // obtenemos la tarea que se ha pulsado en el recycler view apartir de su posicion
+
+        AlertDialog.Builder(this)
+            .setTitle("Delete task")
+            .setMessage("Are you sure you want to delete this task?")
+            .setPositiveButton("Yes") { _, _ ->
+                taskDAO.delete(task) // usamos la funcion delete de la clase TaskDAO para borrar la tarea de la base de datos
+                refreshData() // por ultimo llamamos a la funcion que refresca los datos del recycler view
+            }
+            .setNegativeButton(android.R.string.cancel, null)// no haremos nada-- usamos R.string.cancel porque ya existe en el android  y el mismo lo traduce
+            .setCancelable(false)// esto es para que si pulsamos fuera del dialogo no se cierre
+            .show()
+
+
     }
 }
